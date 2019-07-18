e81d2f89c40327a8d59a42b4105e31841376269f
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2019-07-18 07:53:47 +0200

    Make Value support other Value in input

--- core/Value.py
+++ core/Value.py
@@ -1,15 +1,28 @@
 class Value:
   def __init__(self, val):
-    self.__n = 1
-    self.__sum = val
-    self.__min = val
-    self.__max = val
+    if isinstance(val, Value):
+      self.__n = val.__n
+      self.__sum = val.__sum
+      self.__min = val.__min
+      self.__max = val.__max
+    else:
+      self.__n = 1
+      self.__sum = val
+      self.__min = val
+      self.__max = val
+
 
   def add(self, val):
-    self.__n += 1
-    self.__sum += val
-    self.__min = min(self.__min, val)
-    self.__max = max(self.__max, val)
+    if isinstance(val, Value):
+      self.__n += val.__n
+      self.__sum += val.__sum
+      self.__min = min(self.__min, val.__min)
+      self.__max = max(self.__max, val.__max)
+    else:
+      self.__n += 1
+      self.__sum += val
+      self.__min = min(self.__min, val)
+      self.__max = max(self.__max, val)
 
   def get_mean(self):
     return self.__sum / self.__n
